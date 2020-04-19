7ddc91b1090d948d74dbce16d0ae4c1bd95360a6
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-04-19 18:27:54 +1000

    Black reformat.

--- 01-requests-net-api.py
+++ 01-requests-net-api.py
@@ -5,17 +5,16 @@ import pprint
 
 # Define variables
 # Define the hostname to be queried
-hostname = 'lab-csr-01.lab.dfjt.local'
+hostname = "lab-csr-01.lab.dfjt.local"
 # Define the URL
-url = 'http://localhost:5000'
+url = "http://localhost:5000"
 # Define the API path
-user_host_api = '/api/nr/users/host?host='
+user_host_api = "/api/nr/users/host?host="
 # Define a list of allowed usernames
-allowed_users = ['admin', 'svc-ansible', 'svc-netbox-napalm']
+allowed_users = ["admin", "svc-ansible", "svc-netbox-napalm"]
 
 # Set indentation on pretty prinr
 pp = pprint.PrettyPrinter(indent=2)
-
 
 
 # Get the results of the API call and extract the JSON response
@@ -52,10 +51,10 @@ Below is the JSON data which is presented:
 Below we will access the usernames in the structure
 """
 # Access the username level of the dictionary
-user_list = req[hostname]['users']
+user_list = req[hostname]["users"]
 # Debug pretty print
 # pp.pprint(user_list)
-# Create an empty list, for the username(sP on the device to be entered into
+# Create an empty list, for the username(s) on the device to be entered into
 configured_users = list()
 # Iterate over the keys in the list, which are the username(s)
 for key in user_list.keys():
@@ -66,12 +65,13 @@ for key in user_list.keys():
 allowed_users.sort()
 configured_users.sort()
 
-# If/else list comparision to check that allowed usernames exactly match what is on the device.
-if configured_users == allowed_users: 
-    print ("SUCCESS : The configured users are compliant")
+# If/else list comparision to check that allowed
+# usernames exactly match what is on the device.
+if configured_users == allowed_users:
+    print("SUCCESS : The configured users are compliant")
     print("Compliant user list : " + str(allowed_users))
     print("Actual user list : " + str(configured_users))
-else : 
-    print ("FAILURE : The configured users are compliant")
+else:
+    print("FAILURE : The configured users are compliant")
     print("Compliant user list : " + str(allowed_users))
     print("Actual user list : " + str(configured_users))
