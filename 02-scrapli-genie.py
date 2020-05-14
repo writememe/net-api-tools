442d3cda2117ed3b2bf782162682ba09dd690dbf
Daniel Teycheney <danielfjteycheney@gmail.com>
2020-05-14 20:51:00 +1000

    Add argparse input and colorama.

--- 02-scrapli-genie.py
+++ 02-scrapli-genie.py
@@ -3,22 +3,27 @@ import requests
 import pprint
 from requests.exceptions import HTTPError
 from colorama import Fore, init
+import argparse
 
 # Auto-reset colorama colours back after each print statement
 init(autoreset=True)
 
-# http://localhost:5000/api/nr/scrapli/host?host=lab-csr-01.lab.dfjt.local&command=sddhfhfhfhf
+# Setup argparse parameters to take user input from the command line
+parser = argparse.ArgumentParser(description="Validate startup and running config differences on Cisco devices.")
+parser.add_argument('--host', action='store',help='Specify the hostname to be queried i.e. lab-host-01.lab.local', type=str)
+# Add debug argument, set to False by default unless required
+parser.add_argument('--debug', action='store_true',help='Provide additional debugging output. True for debugging, set to False by default')
+args = parser.parse_args()
 
+print(Fore.CYAN + f"Validating config differences on {args.host}")
+# Take argparse input and assign to the hostname variable
+hostname = args.host
 # Define variables
-# Define the hostname to be queried
-hostname = "lab-csr-01.lab.dfjt.local"
 # Define the URL
 url = "http://localhost:5000"
 # Define the API path
 api_path = "/api/nr/scrapli/host?host="
 command_api_path = "&command="
-# The command to be parsed. Use %20 for spaces
-# command = "show%20archive%20config%20differences"
 # The command to be parsed. Use %20 for spaces
 command = "show%20archive%20config%20differences%20nvram:startup-config%20system:running-config"
 # Set indentation on pretty print
@@ -26,8 +31,15 @@ pp = pprint.PrettyPrinter(indent=2)
 
 #Try/except block to validate a successful HTTP response code
 try:
+  # If debug is set to True, provide the full API call string
+  if args.debug is True:
+    print(Fore.MAGENTA + f"Attempting API call - {url}{api_path}{hostname}{command_api_path}{command}")
   # Get the results of the API call
   req = requests.get(url + api_path + hostname + command_api_path + command)
+  # If debug is set to True, printout the status code and the raw text response
+  if args.debug is True:
+    print(Fore.MAGENTA + f"Response Code - {req.status_code}")
+    print(Fore.MAGENTA + f"Response Raw Text - {req.text}")
   # If the response was successful, no exception will be raised
   req.raise_for_status()
 # Raise exception, print HTTP error
